bed98d6f1431ed65772d708d84508fc135be6c85
Chetan Chandane <chetanchandane10@gmail.com>
2024-12-01 15:33:08 -0500

    Add subscription details getter

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -25,6 +25,4 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
         next()
     } catch (error) {
         throw new ApiError(401, error?.message || "Invalid Access Token!")
-        
-    }
-})
+   
