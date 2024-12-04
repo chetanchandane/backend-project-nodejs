c6a4b4533ad590ce8fed1a9ad63bece5c63f561e
Chetan Chandane <chetanchandane10@gmail.com>
2024-12-04 18:57:44 -0500

    add publish video functionality

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -84,6 +84,55 @@ const getAllVideos = asyncHandler(async (req, res) => {
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
+    
     // TODO: get video, upload to cloudinary, create video
+    const { title, description} = req.body
+    if([title, description].some((field) => field?.trim() === "")) {
+        throw new ApiError(400, "All Fields are required to publish a video.")
+    }
+    const videoFileLocalPath = req.files?.videoFile[0].path;
+    const thumbnailLocalPath = req.files?.thumbnail[0].path;
+
+    if(!videoFileLocalPath ){
+        throw new ApiError(400, "videoFileLocalPath is required.")
+    }
+    if(!thumbnailLocalPath ){
+        throw new ApiError(400, "thumbnailLocalPath is required.")
+    }
+    const videoFile = await uploadOnCloudinary(videoFileLocalPath)
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath)
+
+    if(!videoFile ){
+        throw new ApiError(400, "videoFile not found.")
+    }
+    if(!thumbnail ){
+        throw new ApiError(400, "thumbnail not found.")
+    }
+    const video = await Video.create({
+        title, 
+        description,
+        duration: videoFile.duration, 
+        videoFile: {
+            url: videoFile.url, 
+            public_id: videoFile.public_id
+        },
+        thumbnail: {
+            url: thumbnail.url,
+            public_id: thumbnail.public_id
+        },
+        owner: req.user?._id,
+        isPublished: false
+    })
+    
+    const videoUpload = await Video.findById(video._id)
+
+    if(!videoUpload ){
+        throw new ApiError(500, "videoUpload has failed..")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, video, "video published successfully.")
+    )
 })
