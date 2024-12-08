524c627cabd147262f045ca7e565dbc8080ebcfc
Chetan Chandane <chetanchandane10@gmail.com>
2024-12-08 17:53:10 -0500

    add subscription.controller.js getUserChannelSubscribers function

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -45,7 +45,76 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
-})
+
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400, "Invalid Channel Id!");
+    }
+
+    channelId = new mongoose.Types.ObjectId(channelId);
+    const subscribers = await Subscription.aggregate([
+        {
+            $match: {
+                channel: channelId
+            }
+        }, 
+        {
+            $lookup:{
+                from: "users", 
+                localField: "subscriber", 
+                foreignField: "_id", 
+                as: "subscriber", 
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "subscriptions", 
+                            localField: "_id", 
+                            foreignField: "_id", 
+                            as: "subscribedToSubscriber", 
+                        },
+                    },
+                    {
+                        $addFields: {
+                            subscribedToSubscriber: {
+                                $cond: {
+                                    if: {
+                                        $in: [channelId, "$subscribedToSubscriber.subscriber", ],
+                                    },
+                                    then: true, 
+                                    else: false,
+                                },
+                            },
+                            {
+                                subscribersCount: {
+                                    $size: "$subscribedToSubscriber",
+                                },
+                            },
+                        },
+                    },
+                ]
+            }
+        },
+        {
+            $unwind: "$subscriber",
+        }, 
+        {
+            $project: {
+                _id: 0, 
+                username: 1, 
+                fullname: 1, 
+                "avatar.url": 1, 
+                subscribedToSubscriber: 1, 
+                subscribersCount: 1,
+            },
+        },
+    ]); 
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, subscribers, "Subscribers fetched Successfully!!")
+    );
+
+});
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
